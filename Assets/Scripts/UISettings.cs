1ccaf6674b7712a2a5f08070d0d3f41322e0cbe7
CircuitLord <circuitlordmc@gmail.com>
2019-09-20 12:18:22 -0500

    Added new settings window and improved timing window logic a bit

---
+++ Assets/Scripts/UISettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class UISettings : MonoBehaviour
+{
+   public GameObject bg;
+   public GameObject window;
+
+
+
+   public void Activate()
+   {
+      bg.SetActive(true);
+      window.SetActive(true);
+   }
+
+   public void Deactivate()
+   {
+      bg.SetActive(false);
+      window.SetActive(false);
+   }
+
+   public void OpenSettingsFile()
+   {
+      System.Diagnostics.Process.Start(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "AppData", "LocalLow", "CircuitCubed", "NotReaper", "NRConfig.json"));
+   }
+   
+}
