8b55e5ad818a7d6ae5f0b063121730dfafa46d19
Jay Faulkner <jayleefaulkner@gmail.com>
2019-09-27 23:38:30 +0100

    fix: inverted offset in timeline
    yep I have no idea how I came to that previous conclusion but there we go! I can't hear offset for shit :D

--- Assets/Scripts/Timing/UITiming.cs
+++ Assets/Scripts/Timing/UITiming.cs
@@ -107,7 +107,7 @@ namespace NotReaper.Timing {
             Int32.TryParse(offsetInput.text, out offset);
 
             timeline.SetTimingModeStats(bpm, offset);
-            timeline.SetOffset(offset *= -1);
+            timeline.SetOffset(offset);
 
             mapperName = RemoveSpecialCharacters(mapperInput.text);
             songName = RemoveSpecialCharacters(songNameInput.text);
