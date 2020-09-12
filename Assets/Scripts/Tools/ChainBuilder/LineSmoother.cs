1e97e4dbf618c0bdbb4ed7da7cc120622fb9f66d
Mettra <Mettra@users.noreply.github.com>
2020-09-12 11:52:04 -0700

    fix file

---
+++ Assets/Scripts/Tools/ChainBuilder/LineSmoother.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NotReaper.Tools.ChainBuilder
+{
+
+	public class LineSmoother : MonoBehaviour
+	{
+		public static Vector3[] SmoothLine(Vector3[] inputPoints, float segmentSize)
+		{
+			//create curves
+			AnimationCurve curveX = new AnimationCurve();
+			AnimationCurve curveY = new AnimationCurve();
+			AnimationCurve curveZ = new AnimationCurve();
+
+			//create keyframe sets
+			Keyframe[] keysX = new Keyframe[inputPoints.Length];
+			Keyframe[] keysY = new Keyframe[inputPoints.Length];
+			Keyframe[] keysZ = new Keyframe[inputPoints.Length];
+
+			//set keyframes
+			for (int i = 0; i < inputPoints.Length; i++)
+			{
+				keysX[i] = new Keyframe(i, inputPoints[i].x);
+				keysY[i] = new Keyframe(i, inputPoints[i].y);
+				keysZ[i] = new Keyframe(i, inputPoints[i].z);
+			}
+
+			//apply keyframes to curves
+			curveX.keys = keysX;
+			curveY.keys = keysY;
+			curveZ.keys = keysZ;
+
+			//smooth curve tangents
+			for (int i = 0; i < inputPoints.Length; i++)
+			{
+				curveX.SmoothTangents(i, 0);
+				curveY.SmoothTangents(i, 0);
+				curveZ.SmoothTangents(i, 0);
+			}
+
+			//list to write smoothed values to
+			List<Vector3> lineSegments = new List<Vector3>();
+
+			//find segments in each section
+			for (int i = 0; i < inputPoints.Length; i++)
+			{
+				//add first point
+				lineSegments.Add(inputPoints[i]);
+
+				//make sure within range of array
+				if (i + 1 < inputPoints.Length)
+				{
+					//find distance to next point
+					float distanceToNext = Vector3.Distance(inputPoints[i], inputPoints[i + 1]);
+
+					//number of segments
+					int segments = (int)(distanceToNext / segmentSize);
+
+					//add segments
+					for (int s = 1; s < segments; s++)
+					{
+						//interpolated time on curve
+						float time = ((float)s / (float)segments) + (float)i;
+
+						//sample curves to find smoothed position
+						Vector3 newSegment = new Vector3(curveX.Evaluate(time), curveY.Evaluate(time), curveZ.Evaluate(time));
+
+						//add to list
+						lineSegments.Add(newSegment);
+					}
+				}
+			}
+
+			return lineSegments.ToArray();
+		}
+
+	}
+
+}
